68f2ffcc20c7b1bce4a9d2afbdd32ce881ea7456
Chtholly <llluna@qq.com>
2018-01-23 17:30:51 +0800

    Updates

--- light/v2gif.js
+++ light/v2gif.js
@@ -10,14 +10,32 @@
       rev((params) => {
         const video = document.querySelector('video')
         if (!video) return
+        const width = video.videoWidth
+        const height = video.videoHeight
+        const duration = video.duration
+        params.gifWidth = Math.floor(params.size * width)
+        params.gifHeight = Math.floor(params.size * height)
+        params.numFrames = Math.floor(duration / params.interval)
+        if (params.time) {
+          params.offset = params.time[0]
+          params.numFrames = Math.floor((params.time[1] - params.time[0]) / params.interval)
+        }
+        const div = document.createElement('div')
+        div.style.color = '#fff'
+        div.innerHTML = 'Loading...'
+        document.body.appendChild(div)
         window.gifshot.createGIF(Object.assign({
-          'video': [video.currentSrc]
+          'video': [video.currentSrc],
+          numWorkers: 4
         }, params),function(obj) {
           if(!obj.error) {
             var image = obj.image,
             animatedImage = document.createElement('img');
             animatedImage.src = image;
-            document.body.appendChild(animatedImage);
+            div.innerHTML = ''
+            div.appendChild(animatedImage);
+          } else {
+            div.innerHTML = 'failed' + JSON.stringify(obj.error)
           }
         });
       })
